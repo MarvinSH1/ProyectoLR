4696f1c171b530379a75b849465b2fab36ca1354
Marvin SH <mardevsh@gmail.com>
2024-12-12 11:37:11 -0600

    modified:   backLR/src/routes/adminRoutes.js

--- backLR/src/routes/adminRoutes.js
+++ backLR/src/routes/adminRoutes.js
@@ -1,10 +1,7 @@
 //backLR/src/Routes/adminRoutes.js
 import Router from "express";
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
 
 const router = express.Router();
-
-router.get("/user", getUsers);
 
 export default router;
